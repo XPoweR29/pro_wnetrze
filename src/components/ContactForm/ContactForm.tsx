36c3746b1bfb1d79a1dafe8f9f8f41bb734565a5
Paweł <pawelzydek29@gmail.com>
2026-02-13 16:58:37 +0100

    chore: set all for release merge

--- src/components/ContactForm/ContactForm.tsx
+++ src/components/ContactForm/ContactForm.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 import styles from './ContactForm.module.scss';
 import { ContactFormData, useContactForm } from '@/hooks/useContactForm';
 import icon_envelope from '../../assets/icons/btn_envelope.svg';
