8e7b262f0f8f532d58fb00814b2c670cbd492bf7
Paweł <pawelzydek29@gmail.com>
2026-02-13 08:33:47 +0100

    feat: add send-email function and type update

---
+++ src/app/actions/send-email.ts
@@ -0,0 +1,43 @@
+'use server';
+
+import ContactTemplate from '@/components/emails/contact-template';
+import { ContactFormData } from '@/hooks/useContactForm';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmail = async (data: ContactFormData) => {
+	const { email, message, name, phone, subject } = data;
+	const formattedPhone = phone ? `+48 ${phone}` : 'Nie podano';
+
+	if (!name?.trim() || !email?.trim() || !message?.trim()) {
+		return { error: `Wypełnij wszystkie wymagane pola` };
+	}
+
+	try {
+		const { data: resendData, error: resendError } = await resend.emails.send({
+			from: 'kontakt@pro-wnetrze.pl',
+			to: ['kontakt@pro-wnetrze.pl', 'ceo.webcraftstudio@gmail.com'],
+			subject: 'Wiadomość z formularza kontaktowego',
+			text: message,
+			react: ContactTemplate({
+				name,
+				email,
+				phone: formattedPhone,
+				message,
+				subject: 'Wiadomość z formularza',
+			}),
+			replyTo: email,
+		});
+
+		if (resendError) {
+			console.error(`Resend Error: ${resendError}`);
+			return { error: resendError.message };
+		}
+
+		return { success: true, data: resendData };
+	} catch (error) {
+		console.error(`Server Action Error: ${error}`);
+		return { error: `Wystąpił bład podczas wysyłania wiadomości.` };
+	}
+};
